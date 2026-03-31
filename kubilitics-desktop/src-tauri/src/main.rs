f6fc39958c21fcb788dde643ed7677933e0e738a
Koti <vellankikoti@gmail.com>
2026-03-31 12:52:57 +0530

    fix: use tauri_plugin_updater::Builder::new().build() (not init())

--- kubilitics-desktop/src-tauri/src/main.rs
+++ kubilitics-desktop/src-tauri/src/main.rs
@@ -14,7 +14,7 @@ fn main() {
         .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_dialog::init())
-        .plugin(tauri_plugin_updater::init())
+        .plugin(tauri_plugin_updater::Builder::new().build())
         .invoke_handler(tauri::generate_handler![
             commands::read_kubeconfig,
             commands::get_kubeconfig_info,
